38d1a99b3e8f23b5a4b4d3d94dadcd2287f10569
Joseph DeCrisanti <joseph_decrisanti@icloud.com>
2019-10-24 16:44:01 -0400

    Initial Commit

---
+++ CIS 240 Final/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CIS 240 Final
+//
+//  Created by Joseph DeCrisanti on 10/24/19.
+//  Copyright © 2019 Joseph DeCrisanti. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
