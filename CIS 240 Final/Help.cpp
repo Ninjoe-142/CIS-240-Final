d03c9db968d02f2d63e6864255d2b6a4137e1dd7
Joseph DeCrisanti <joseph_decrisanti@icloud.com>
2019-12-04 16:42:40 -0500

    File Creation

---
+++ CIS 240 Final/Help.cpp
@@ -0,0 +1,9 @@
+//
+//  Help.cpp
+//  CIS 240 Final
+//
+//  Created by Joseph DeCrisanti on 12/4/19.
+//  Copyright © 2019 Joseph DeCrisanti. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ CIS 240 Final/Questions.cpp
@@ -0,0 +1,8 @@
+//
+//  Questions.cpp
+//  CIS 240 Final
+//
+//  Created by Joseph DeCrisanti on 12/4/19.
+//  Copyright © 2019 Joseph DeCrisanti. All rights reserved.
+//
+
