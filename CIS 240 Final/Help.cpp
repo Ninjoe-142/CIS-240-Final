8ad0b4da5829899e3adacf36c02b3cdcfe0038fd
Fortiek <46032953+Fortiek@users.noreply.github.com>
2019-12-05 02:24:04 -0500

    Update 1 Help.cpp
    just couts

--- CIS 240 Final/Help.cpp
+++ CIS 240 Final/Help.cpp
@@ -1,9 +1,24 @@
-//
-//  Help.cpp
-//  CIS 240 Final
-//
-//  Created by Joseph DeCrisanti on 12/4/19.
-//  Copyright © 2019 Joseph DeCrisanti. All rights reserved.
-//
+/*Help.cpp
+Matt Archer & Joe DeCrisanti
+Dec 5, 2019
+CIS 240.01
+Post University*/
 
-#include <stdio.h>
+void Help()
+{
+	//cout statements telling user how to use game
+	cout << "******************************\n"
+		<< "*            HELP             *\n"
+		<< "*******************************\n";
+	cout << endl << endl;
+
+	cout << "To start the game and answer each\n"
+		<< "subsiquent question, enter the \n"
+		<< "designated characters associated \n"
+		<< "with menu options, (Like 'G', 'H',\n"
+		<< "or 'S'.) or with the appropriate\n"
+		<< "answer. ('A', 'B', 'C' or 'D'.)\n"
+		<< "Good luck, and may RNGezus have\n"
+		<< "mercy on your soul!";
+
+}
