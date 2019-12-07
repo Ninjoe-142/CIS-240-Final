3d8e4267a403a0427c3ebb7be8e7a58779b76fe1
Fortiek <46032953+Fortiek@users.noreply.github.com>
2019-12-07 16:24:52 -0500

    Update main.cpp

--- CIS 240 Final/main.cpp
+++ CIS 240 Final/main.cpp
@@ -7,34 +7,44 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
+
+
+int numOfUsers; 	         //Global counter for number of users
+const char DEFAULT_MONEY = '0'; //For reseting scores of users
+typedef string PlayerType[10][2];
 
 //function prototypes
 //Game should be void? Not sure, depends on display
 //Questions seems like it could be several functions. Setting as bool return with empty parameters
 void Help();
-void Score();
-void playGame();
+void InputScore(PlayerType, string, string);
+void Game();
 bool Questions();
+void showScore();
 
 int main() {
 	// skeleton function to call other functions
 
-	char selection; //For menu seletion
+	char selection;  //For menu seletion
+	PlayerType playerTable; //To initialize table
 
 	cout << "******************************\n"
 		<< "  Welcome to the Final game!  \n"
 		<< "******************************\n";
 
 	cout << endl << endl;
-
+	 
 	//Menu choices
 	cout << "Start by selecting an option\n"
 		<< "from the following:\n"
 		<< endl
 		<< "'G': Jump right in to the game\n"
 		<< "'H': Explanation to the game\n"
-		<< "'S': Access score menu\n";
+		<< "'S': Access score menu(disabled)\n"
+		<< "'T': Check current leaderboard\n";
 
 	cin >> selection;
 
@@ -42,19 +52,24 @@ int main() {
 	switch (selection)
 	{
 	case 'g':
-		case 'G': playGame();
+	case 'G': Game();
 		break;
 	case 'h':
 	case 'H': Help();
 		break;
-	case 's':
-	case 'S': //Score();
+	/*case 's':
+	case 'S': InputScore(playerTable, string for playerName, string for score *FROM PLAYGAME()* );
+		break;*/
+	case 't':
+	case 'T': showScore();
 		break;
 	default: cout << "Please enter 'G' for the game, 'H' for help\n"
-			<< "or 'S' to access the score menus.\n";
+		<< "or 'S' to access the score menus.\n";
 		cin >> selection;
 		break;
 	}
+
+	system("PAUSE");
 
 	return 0;
 }
